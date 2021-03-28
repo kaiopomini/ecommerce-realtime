b43737922323d173db05f776c8242ac177038d27
Kaio Pomini <kaiopomini@gmail.com>
2021-03-28 02:43:59 -0300

    Auth controller and some fixes on models

--- database/seeds/0001_RoleSeeder.js
+++ database/seeds/0001_RoleSeeder.js
@@ -15,26 +15,44 @@ const Role = use('Role')
 class RoleSeeder {
   async run () {
 
-    // Cria admin role
-    await Role.create({
-      name: 'Admin',
-      slug: 'admin',
-      description: 'Administrador do sistema'
-    })
+    const admin = new Role()
+    admin.name = 'Admin'
+    admin.slug = 'admin'
+    admin.description = 'Administrador do Sistema'
+    await admin.save()
 
-    // Cria o cargo de gerente
-    await Role.create({
-      name: 'Manager',
-      slug: 'manager',
-      description: 'Gerente da loja'
-    })
+    const manager = new Role()
+    manager.name = 'Gerente'
+    manager.slug = 'manager'
+    manager.description = 'Manager do Sistema'
+    await manager.save()
 
-    // Cria o cargo Cliente
-    await Role.create({
-      name: 'Client',
-      slug: 'client',
-      description: 'Cliente da loja'
-    })
+    const client = new Role()
+    client.name = 'Cliente'
+    client.slug = 'client'
+    client.description = 'Cliente do Sistema'
+    await client.save()
+
+    // // Cria admin role
+    // await Role.create({
+    //   name: 'Admin',
+    //   slug: 'admin',
+    //   description: 'Administrador do sistema'
+    // })
+
+    // // Cria o cargo de gerente
+    // await Role.create({
+    //   name: 'Manager',
+    //   slug: 'manager',
+    //   description: 'Gerente da loja'
+    // })
+
+    // // Cria o cargo Cliente
+    // await Role.create({
+    //   name: 'Client',
+    //   slug: 'client',
+    //   description: 'Cliente da loja'
+    // })
   }
 }
 
