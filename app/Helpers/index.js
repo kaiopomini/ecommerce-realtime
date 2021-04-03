d0d51f862b62bc288ec23554b74a1d5f0ac6cd2f
Kaio Pomini <kaiopomini@gmail.com>
2021-04-03 16:58:17 -0300

    Helpers

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -27,6 +27,64 @@ const str_random = async (length = 40) => {
   return string
 }
 
+/**
+ * Move um unico arquivo para o caminho especificado, se nenhum for especificado
+ * entao 'public/uploads' será utilizado.
+ * @param { FileJar } file o arquivo a ser gerenciado
+ * @param { string } path o caminho para onde o arquivo deve ser movido
+ * @return { Object<fileJar> }
+ */
+
+const manage_single_upload = async (file, path=null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+
+  //gera um numero aleatorio
+  const random_name = await str_random(30)
+  let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+  //renomeia o arquivo e move para o Path
+  await file.move(path, {
+    name: filename
+  })
+
+  return file
+}
+
+/**
+ * Move multiplos arquivos para o caminho especificado, se nenhum for especificado
+ * entao 'public/uploads' será utilizado.
+ * @param { FileJar } file o arquivo a ser gerenciado
+ * @param { string } path o caminho para onde o arquivo deve ser movido
+ * @return { Object }
+ */
+const manage_multiple_uploads = async (fileJar, path = null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+  let successes = [], errors = []
+
+  await Promise.all(fileJar.files.map(async file=> {
+    let random_name = await str_random(30)
+    let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+    //move o arquivo
+    await file.move(path, {
+      name: filename
+    })
+
+    //verifica se o arquivo foi movido
+    if(file.moved()){
+      successes.push(file)
+    } else {
+      errors.push(file.errors())
+    }
+
+  }))
+
+  return { successes, errors }
+}
+ 
+
 module.exports = {
-  str_random
+  str_random,
+  manage_multiple_uploads,
+  manage_single_upload
 }
